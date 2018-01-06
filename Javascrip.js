fc5b183a9a09f785105a7d1261a3be1b32661982
Simon <seedsimo@live.trinity.sa.edu.au>
2018-01-06 22:08:59 +0000

    Added quite

--- Javascrip.js
+++ Javascrip.js
@@ -6,9 +6,18 @@ function menu(){
 	document.getElementById("menu_button").style.display="none";
 	document.getElementById("resume").style.display="block";
 	document.getElementById("save").style.display="block";
+	document.getElementById("quit").style.display="block";
 }
 function resume(){
 	document.getElementById("menu_button").style.display="block";
 	document.getElementById("resume").style.display="none";
 	document.getElementById("save").style.display="none";
+	document.getElementById("quit").style.display="none";
+}
+function quit(){
+	document.getElementById("menu_button").style.display="none";
+	document.getElementById("resume").style.display="none";
+	document.getElementById("save").style.display="none";
+	document.getElementById("quit").style.display="none";
+	document.getElementById("start").style.display ="block";
 }
